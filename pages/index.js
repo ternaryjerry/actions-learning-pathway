0caf9b288e39549299dfe6842d653ec2d5749624
Jerry Evilsizer <96720410+ternaryjerry@users.noreply.github.com>
2024-05-04 13:31:11 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -17,12 +17,8 @@ export default function Home() {
       <main className={`${styles.main} ${inter.className}`}>
 
        <h1>Congratulations!</h1>
-    <div>
-      <p>You've completed the essentials of automated application deployment with GitHub Actions! 🥳</p>
-      <div style="width:100%;height:0;padding-bottom:75%;position:relative;"><iframe src="https://giphy.com/embed/W2oHQI3Y8Vu7IcI6k1" width="100%" height="100%" style="position:absolute" frameBorder="0" class="giphy-embed" allowFullScreen></iframe></div><p><a href="https://giphy.com/gifs/bosch-power-tools-electric-W2oHQI3Y8Vu7IcI6k1">via GIPHY</a></p>
-  </div>
-
-  <iframe src="https://giphy.com/embed/W2oHQI3Y8Vu7IcI6k1" width="480" height="360" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/bosch-power-tools-electric-W2oHQI3Y8Vu7IcI6k1">via GIPHY</a></p>
+        <p>You've completed the essentials of automated application deployment with GitHub Actions! 🥳</p>
+        <iframe src="https://giphy.com/embed/W2oHQI3Y8Vu7IcI6k1" width="480" height="360" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/bosch-power-tools-electric-W2oHQI3Y8Vu7IcI6k1">via GIPHY</a></p>
       </main>
     </>
   )
